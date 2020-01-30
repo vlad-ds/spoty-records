f17b6504320e32d4dd03fca175a49be69ba99d3d
Vlad Gheorghe <contact@vlad-gheorghe.com>
2020-01-30 18:52:25 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Created on Wed Jan 29 10:29:55 2020
+
+@author: vlad
+"""
+
+#insert local parameters
+
+user = 'your-spotify-username'
+client_id ='your-client-id'
+client_secret = 'your-client-secret'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-recently-played'
