76247dbab7f706a66f89f3b95a6d68830ed9e9db
rebeccasomerville <128788967+rebeccasomerville@users.noreply.github.com>
2023-05-09 00:09:41 -0700

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ Created on Wed Jan 29 10:29:55 2020
 
 #insert local parameters
 
-username = 'your-spotify-username'
-client_id ='your-client-id'
-client_secret = 'your-client-secret'
+username = '9dl3fuh7jqn8npl4lf95v6o4p'
+client_id ='48b6e5d09e634f408140ab70e3021118'
+client_secret = 'd0a901be9fa34921a242b925d8a16d94'
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
